452a402b10b6edc8fa19e28cc08410820b43448e
Patryk Kubiczek <patryk.kubiczek@gmail.com>
2021-11-27 11:47:18 +0100

    Download scripts added: USDA

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -18,6 +18,7 @@ fao_filenames = [
     "Food_Security_Data_E_All_Data",
     "FoodBalanceSheets_E_All_Data",
     "Prices_E_All_Data",
+    "SUA_Crops_Livestock_E_All_Data",
     "Trade_CropsLivestock_E_All_Data",
     "Trade_DetailedTradeMatrix_E_All_Data",
     "Value_of_Production_E_All_Data",
@@ -52,6 +53,7 @@ if not dest.exists():
                 df = df[area_filter | reporter_partner_filter]
                 dfs.append(df)
     df = pd.concat(dfs)
+    df = df[~df.duplicated()]
 
     df.to_csv(data_dir("processed", f"fao-data-{country}.csv"), index=False)
 
@@ -79,7 +81,46 @@ dtype = {
 
 df = pd.read_csv(dest, dtype=dtype)
 
-print(df.dtypes)
+df = df[df.duplicated(keep='last')]
+
+print(df["Element"].unique())
+
+
+## USDA PSD
+
+usda_url = r"https://apps.fas.usda.gov/psdonline/downloads/"
+
+usda_filenames = [    
+    "psd_alldata_csv",
+]
+
+os.makedirs(data_dir("raw", "usda"), exist_ok=True)
+
+for filename in usda_filenames:
+    dest = data_dir("raw", "usda", filename + ".csv")
+    if dest.exists():
+        continue
+    url = usda_url + filename + ".zip"
+    print(f"Downloading and unzipping {url}")
+    r = requests.get(url, stream=True)
+    zip_file = ZipFile(BytesIO(r.content))
+    extracted_name = zip_file.namelist()[0]
+    zip_file.extractall(path=dest.parent)
+    os.rename(data_dir("raw", "usda", extracted_name), dest)
+
+# Filter and save
+dest = data_dir("processed", f"usda-data-{country}.csv")
+if not dest.exists():
+    dfs = []
+    for filename in usda_filenames:
+        file = data_dir("raw", "usda", filename + ".csv")
+        with pd.read_csv(file, chunksize=1e6, encoding="latin-1", dtype="string") as reader:
+            for df in reader:
+                df = df[df["Country_Name"] == country]
+                dfs.append(df)
+    df = pd.concat(dfs)
+
+    df.to_csv(data_dir("processed", f"usda-data-{country}.csv"), index=False)
 
 
 ## IDB
